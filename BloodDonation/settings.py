12e9884c1195b553c848b6ce7697fa9f33837ef3
Nitay_Das <nitaydas910@gmail.com>
2024-05-10 01:41:35 +0600

    changes for deployment

--- BloodDonation/settings.py
+++ BloodDonation/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = 'wy#+(&hxnf5$hd=wx)0mffwknu5h+@=u7&e%e%v8nlj!bd*4)l'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Application definition
